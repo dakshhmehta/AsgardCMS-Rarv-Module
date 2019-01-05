1b70c4dd246ff086a36933fcce3a17bac420efe1
Daksh Mehta <daksh@dristal.com>
2019-01-05 20:28:55 +0530

    Removed min date validation

--- Resources/views/partials/form/date_group.blade.php
+++ Resources/views/partials/form/date_group.blade.php
@@ -1,10 +1,12 @@
+{{-- TODO: Implement min, max --}}
+
 @stack($name . '_input_start')
 
 <div class="form-group {{ $col }} {{ isset($attributes['required']) ? 'required' : '' }} {{ $errors->has($name) ? 'has-error' : '' }}">
     {!! Form::label($name, $text, ['class' => 'control-label']) !!}
     <div class="input-group">
         <div class="input-group-addon"><i class="fa fa-{{ $icon }}"></i></div>
-        {!! Form::date($name, $value, array_merge(['class' => 'form-control', 'placeholder' => $text, 'min' => carbon()->format('Y-m-d')], $attributes)) !!}
+        {!! Form::date($name, $value, array_merge(['class' => 'form-control', 'placeholder' => $text], $attributes)) !!}
     </div>
     {!! $errors->first($name, '<p class="help-block">:message</p>') !!}
 </div>
--- includes/helpers.php
+++ includes/helpers.php
@@ -8,22 +8,3 @@ if (!function_exists('carbon')) {
         return Carbon::now();
     }
 }
-
-if (!function_exists('currency')) {
-    /**
-     * Format the amount in decimal
-     * into Indian currency
-     * @param  decimal $amount
-     * @return string
-     *
-     * @author Daksh Mehta <dm@rimail.in>
-     */
-    function currency($amount)
-    {
-        if ($amount != null) {
-            return '&#x20b9; ' . number_format($amount, 2) . '/-';
-        } else {
-            return '&#x20b9; ' . number_format(0, 2) . '/-';
-        }
-    }
-}
