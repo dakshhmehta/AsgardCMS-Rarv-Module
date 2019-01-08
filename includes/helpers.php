2cee95e7d32457705ad3af45fa822b5076e04e57
Daksh Mehta <daksh@dristal.com>
2019-01-08 20:23:50 +0530

    Added helper method for the asset manager

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,10 +1,17 @@
 <?php
 
 use Carbon\Carbon;
+use Modules\Core\Foundation\Asset\Pipeline\AssetPipeline;
 
 if (!function_exists('carbon')) {
     function carbon()
     {
         return Carbon::now();
     }
+}
+
+if(! function_exists('assetManager')){
+	function assetManager(){
+		return app(AssetPipeline::class);
+	}
 }
