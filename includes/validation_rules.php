74aeb8e04731379275b1e295a3876f872a6d6742
Daksh Mehta <daksh@dristal.com>
2019-01-09 12:39:47 +0530

    Validation rule for gt or equals

--- includes/validation_rules.php
+++ includes/validation_rules.php
@@ -7,6 +7,13 @@ Validator::extend('greater_than_field', function ($attribute, $value, $parameter
     return $value > $min_value;
 });
 
+Validator::extend('greater_equals_than_field', function ($attribute, $value, $parameters, $validator) {
+    $min_field = $parameters[0];
+    $data      = $validator->getData();
+    $min_value = $data[$min_field];
+    return $value >= $min_value;
+});
+
 Validator::replacer('greater_than_field', function ($message, $attribute, $rule, $parameters) {
     return str_replace(':field', $parameters[0], $message);
 });
