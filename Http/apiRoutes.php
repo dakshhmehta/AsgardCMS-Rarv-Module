43cf6f73a05e51535fc4a44add609d7e8519dc90
Daksh Mehta <daksh@dristal.com>
2019-11-07 16:11:50 +0530

    Added route as well

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -1,8 +1,13 @@
 <?php
+Route::post('login', [
+    'as'   => 'api.rarv.user.login',
+    'uses' => 'AuthController@postLogin',
+]);
 
 $router->group(
     ['prefix' => '/rarv', 'middleware' => ['api.token']],
     function ($router) {
+
         $router->post('events/trigger/{action}/{id}', 'EventsController@trigger');
     }
 );
